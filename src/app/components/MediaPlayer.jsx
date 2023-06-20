325729501d98e96dc4f952665ecc105805ed40c5
Agit Naeta <agitnaeta@192.168.1.9>
2023-06-20 20:09:37 +0700

    Video 1 - 5

---
+++ src/app/components/MediaPlayer.jsx
@@ -0,0 +1,5 @@
+export default function MediaPlayer(){
+    return (
+        <p className="bg-black">Sedang Memutar ... </p>
+    )
+}
---
+++ src/app/components/MusicList.jsx
@@ -0,0 +1,12 @@
+export default function MusicList(){
+    return(
+        <div className="bg-black text-white">
+            <h3>Music List</h3>
+            <ul>
+                <li>Music 1</li>
+                <li>Music 2</li>
+                <li>Music 3</li>
+            </ul>
+        </div>
+    )
+}
